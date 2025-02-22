dc303eb5a2d5de79425cd3890a7b77d4d2b75933
陳勇涵 <hanchen@chenyonghandeMacBook-Pro.local>
2025-02-22 16:56:33 +0800

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -291,7 +291,10 @@ function App() {
 						<div className="col-12">
               <div className="d-flex justify-content-between align-items-center my-3">
                 <h2 className="text-center">產品列表</h2>
-                <button type="button" className='btn btn-primary' onClick={() => {handleOpenProductModal('add', modalDefaultData)}}>建立新的產品</button>
+                <div className="d-flex align-items-center gap-2">
+                  <button type="button" className='btn btn-danger' onClick={handleLogOut}>登出</button>
+                  <button type="button" className='btn btn-primary' onClick={() => {handleOpenProductModal('add', modalDefaultData)}}>建立新的產品</button>
+                </div>
               </div>
 							<table className="table">
 								<thead>
